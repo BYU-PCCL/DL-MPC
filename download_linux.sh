131c3a15ed82ff2decb8679ed60ca52b31ce6be2
Taylor Sorensen <taylorsorensen@Taylors-MacBook-Pro.local>
2021-03-16 11:29:09 -0600

    download_linux.sh

---
+++ download_linux.sh
@@ -0,0 +1,9 @@
+# downloaod model parameters
+wget https://ndownloader.figshare.com/files/26840927
+unzip 26840927
+rm 26840927
+
+# download data
+wget https://ndownloader.figshare.com/files/26841041
+unzip 26841041
+rm 26841041
